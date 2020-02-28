5c278016d1c250e8f4212a49040f642c2baa293e
Jessica Hart <jessicahart@Jessicas-MacBook-Pro.local>
2020-02-28 11:15:09 -0800

    portfolio version 1.0

--- practice/javascript/week6/ifstatements.js
+++ practice/javascript/week6/ifstatements.js
@@ -29,3 +29,50 @@ var isSleeply = false;
 if (isSleeply) {
     console.log ("go home and");
 }
+
+var test = "1000";
+if (test == 1000) {
+    console.log("test!");
+}
+
+//Type coercin: Forces a datatype to be another datatype.
+// follow allow type coercin: == >= <= !=
+//these do not allow type coercion: === >== <== !==
+var sayHello;
+if (sayHello) {
+    console.log("hello");
+}
+else {
+    console.log("goodbye");
+}
+
+
+//truthy and falsy values.
+//if a variable is holding any of the following it will become a false boolean:
+/*
+list of most common falsys
+Falsy
+undefined
+null
+0
+""
+NaN
+list of most common truthys
+"0"
+-1
+*/
+var checkIfTrue = "";
+if (checkIfTrue) {
+    console.log("the " + checkifTrue + " value is truthy");
+} else {
+    console.log("the " + checkIfTrue + " value is falsy");
+}
+
+
+//logical operators: &&,||,!
+// && || !=
+var firstBool = true;
+var secondBool = false;
+if (firstBool && secondBool) {
+    console.log("true");
+}
