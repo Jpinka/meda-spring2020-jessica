e29cc2f28eeea39c1a9829e199b8b61ce38e04a0
Jessica Hart <jessicahart@Jessicas-MacBook-Pro.local>
2020-02-28 12:21:30 -0800

    new portfolio version 1.0

--- practice/javascript/week6/loops.js
+++ practice/javascript/week6/loops.js
@@ -1,1 +1,25 @@
 console.log("Testing");
+
+//WHile loop "while (condition) {code}"
+//A loop needs a counter so we can track how many times it has loop.
+var counter = 0;
+
+while (counter < 10) {
+    //console.log(number);
+    console.log("hello");
+    counter = counter + 1;
+
+
+}; 
+
+//Do While loop "do {} while ();"
+
+
+var counter2 = 0;
+//do the code first...
+do {
+    console.log("goodbye");
+    counter2 = counter2 + 1;
+//then check to see if we do it again
+}
+while(false);
